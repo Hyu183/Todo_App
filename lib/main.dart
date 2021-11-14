07bc1f12b91a7c8fefdb2c7dd12942cfdc0d4b64
l4zyc0d3r <huytrquang1103@gmail.com>
2021-11-14 20:11:51 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Todo'),
+    );
+  }
+}
+
+class MyHomePage extends  StatelessWidget {
+  const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+} {
+  
+}
+
